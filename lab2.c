2513593cb4d5b89a9e34ef1d20da437b62e8c8d2
Cameron Hoffman <70237136+CHoffman2020@users.noreply.github.com>
2024-03-24 18:55:47 -0400

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 #include <sys/time.h>
+#include <sys/times.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
@@ -10,28 +11,61 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <errno.h>
+#include <aio.h>
 
 // cd ~/src/lab2
 // gcc -g -pthread -o lab2 lab2.c
 
-int SIZE = 15;
+int SIZE = 10000000;
 char ALT = 'S';		  // S
 int THRESH = 10;	  // 10
 int SEED = 0;		  // Random
 char MULTHREAD = 'Y'; // Y
-int PIECES = 4;	  // 10
+int PIECES = 10;	  // 10
 int MAXTHREAD = 4;	  // 4
 char MED3 = 'N';	  // N
-char EARLY = 'N';	  // Y
+char EARLY = 'Y';	  // Y
 int *array;
+
+double totalLoadTime, totalSortTime, totalTotalTime, totalCPUTime;
+bool validStart = false, validEnd = false;
+
+struct timeval loadTimeStart, loadTimeEnd;
+struct timeval sortTimeStart, sortTimeEnd;
+struct timeval totalTimeStart, totalTimeEnd;
+struct tms CPU_TimeStart, CPU_TimeEnd;
+
+typedef struct
+{
+	int start;
+	int end;
+	int *data;
+}
+_Thread;
+
+//pthread_t *ID;
+//_Thread *tArray;
 
 #define FILENAME "random.dat"
 // ADD RANDOM FILE ONCE U GET THE SORTING WORKING
 bool isSorted(int *sArray, int Num);
 void QuickSort(int *qarray, int lo, int hi);
+void *Threading (void *ThreadArray);
+
 
 int main(int argc, char *argv[], char *env[])
 {
+	while(!validStart)
+	{
+		if(gettimeofday(&totalTimeStart, NULL) != 0)
+		{
+			continue;
+		}
+		if(totalTimeStart.tv_sec > 0 || totalTimeStart.tv_usec > 0) {
+            validStart = true;
+        }
+	}
+	validStart = false;
 	for (int i = 1; i < argc; i += 2)
 	{
 		if (strcmp(argv[i], "-n") == 0)
@@ -117,7 +151,8 @@ int main(int argc, char *argv[], char *env[])
 		}
 		else
 		{
-			printf("\nERROR");
+			printf("\nNO INPUTS GIVEN");
+			return 0;
 		}
 	}
 	printf("\n");
@@ -128,13 +163,31 @@ int main(int argc, char *argv[], char *env[])
 	srand(time(NULL));
 	// GRABBING A SPECIFIC CHUNK OF THE MEMORY IN THE FILE AND CONVERTING THROWING THEM INTO AN array
 	// ALSO HANDLED STARTING AT THE END OF A FILE AND WRAPPING BACK AROUND TO THE FRONT
+	//clock_gettime(CLOCK_MONOTONIC, &totalTimeStart);
+	//clock_gettime(CLOCK_MONOTONIC, &loadTimeStart);
+
+	//gettimeofday(&totalTimeStart, NULL);
+
+	while(!validStart)
+	{
+		if(gettimeofday(&loadTimeStart, NULL) != 0)
+		{
+			continue;
+		}
+		if(loadTimeStart.tv_sec > 0 || (loadTimeStart.tv_sec == 0 && loadTimeStart.tv_usec > 0)) {
+            validStart = true;
+        }
+	}
+	validStart = false;
+	
+	//gettimeofday(&loadTimeStart, NULL);
+
 	array = (int *)malloc(SIZE * sizeof(int));
 	if (array == NULL)
 	{
 		fprintf(stderr, "Memory allocation failed\n");
 		return 1; // Exit the program if memory allocation fails
 	}
-
 	FILE *inputFile = fopen(FILENAME, "rb");
 	if (!inputFile)
 	{
@@ -148,8 +201,25 @@ int main(int argc, char *argv[], char *env[])
 		int leftOverData = SIZE - dataCount;
 		fread(array + dataCount, sizeof(int), leftOverData, inputFile);
 	}
-
+	//int x = (int)(loadTimeStart.tv_sec + loadTimeStart.tv_usec);
+	//int y = (int)(loadTimeEnd.tv_sec + loadTimeEnd.tv_usec);
+	//printf("Start: %d\nEnd: %d\n", x, y);
 	fclose(inputFile);
+
+	while(!validEnd)
+	{
+		if(gettimeofday(&loadTimeEnd, NULL) != 0)
+		{
+			continue;
+		}
+		if(loadTimeEnd.tv_sec > 0 || (loadTimeEnd.tv_sec == 0 && loadTimeEnd.tv_usec > 0)) {
+            validEnd = true;
+		}
+	}
+	validEnd = false;
+
+	//gettimeofday(&loadTimeEnd, NULL);
+	//clock_gettime(CLOCK_MONOTONIC, &loadTimeEnd);
 	/*
 	for(int i=0; i < SIZE; i++)
 	{
@@ -182,15 +252,27 @@ int main(int argc, char *argv[], char *env[])
 			}
 		}
 	}
-
+	times(&CPU_TimeStart);
+	//gettimeofday(&sortTimeStart, NULL);
 	// MULTITHREADING WITH AND WITHOUT MEDIAN
 	if (MULTHREAD == 'Y')
 	{
+		while(!validStart)
+		{
+			if(gettimeofday(&sortTimeStart, NULL) != 0)
+			{
+				continue;
+			}
+			if(sortTimeStart.tv_sec > 0 || (sortTimeStart.tv_sec == 0 && sortTimeStart.tv_usec > 0)) {
+				validStart = true;
+			}
+		}
 		printf("Creating multithread partitions\n");
 		int start[PIECES];
 		int end[PIECES];
 		int pivot;
 		//int piece = 1;
+		//clock_gettime(CLOCK_MONOTONIC, &sortTimeStart);
 		for(int piece = 1; piece < PIECES; piece++)
 		{
 			if(piece == 1)
@@ -249,7 +331,6 @@ int main(int argc, char *argv[], char *env[])
 			array[low] = array[j];
 			array[j] = temp;
 
-
 			double RPerc = (high - low + 1 > 0) ? ((double)(high - j - 1) / (double)(high - low + 1)) * 100 : 0.0;
 			double LPerc = (high - low + 1 > 0) ? ((double)(j - 1 - low) / (double)(high - low + 1)) * 100 : 0.0;
 			printf("results: %3d   -  %3d (%2.1f / %2.1f)\n", j - 1 - low, high - j + 1, LPerc, RPerc);
@@ -276,32 +357,157 @@ int main(int argc, char *argv[], char *env[])
 				}
 			}
         }
-		/*
-		for (int i = 0; i < PIECES; i++)
+		for(int i = 0; i < PIECES; i++)
 		{
-			printf("PIECE: %d  |   %d  -  %d\n", i, start[i], end[i]);
+			int pieceSize = (end[i] - start[i]);
+			double piecePerc = (pieceSize != 0) ? ((double)SIZE / pieceSize) : 0.0;
+			printf("%d: %3d -  %3d", i, start[i], end[i]);
+			printf(" ( %3d - %2.1f)\n", pieceSize, piecePerc);
 		}
+		//pthread_t *ID;
+		//_Thread *tArray;
+		pthread_t *ID = malloc(MAXTHREAD * sizeof(pthread_t));	//threads
+		if (ID == NULL)
+		{
+			fprintf(stderr, "Array allocation failed\n");
+			return 1; // Exit the program if memory allocation fails
+		}
+		_Thread *tArray = malloc(MAXTHREAD * sizeof(_Thread)); //Array
+		if (tArray == NULL)
+		{
+			fprintf(stderr, "Array allocation failed\n");
+			return 1; // Exit the program if memory allocation fails
+		}
+		int Counter = 0, sCounter = 0, done = 0, result = 0;
 		
-		for(int i=0; i < SIZE; i++)
-		{
-			printf("%d\n", array[i]);
-		}
-		*/
-		for (int i = 0; i < PIECES; i++)
-		{
-			QuickSort(array, start[i], end[i]);
-			//printf("PARTION %d SORTED\n", i);
-		}
+			if(MAXTHREAD > 0 && MAXTHREAD <= 4)
+			{
+				for(int i = 0; i < MAXTHREAD; i++)
+				{
+					tArray[i].end = end[i];
+					tArray[i].start = start[i];
+					pthread_create(&ID[i], NULL, Threading, (void *)&tArray[i]);
+					printf("Launching thread to sort: %3d -  %3d ", tArray[i].start, tArray[i].end);
+					printf("( %3d): row( %d)\n", tArray[i].end - tArray[i].start, sCounter);
+					Counter++;
+					sCounter++;
+				}
+			}
+			else
+			{
+				printf("MAXTHREAD INPUT IS INVALID\n");
+			}
+			while(done < PIECES)
+			{
+				for(int i = 0; i < MAXTHREAD; i++)
+				{
+					result = pthread_tryjoin_np(ID[i], NULL);
+					if(result == 0)
+					{				
+						done++;
+						Counter++;
+						if(sCounter < PIECES)
+						{
+							tArray[i].end = end[sCounter];
+							tArray[i].start = start[sCounter];
+							pthread_create(&ID[i], NULL, Threading, (void *)&tArray[i]);
+							printf("Launching thread to sort: %3d -  %3d ", tArray[i].start, tArray[i].end);
+							printf("( %3d): row( %d)\n", tArray[i].end - tArray[i].start, sCounter);
+							Counter++;
+							sCounter++;
+						}
+					}
+					else if(done == EBUSY)
+					{
+						continue;
+					}
+				}
+				usleep(100000);
+			}
+			free(tArray);
+			free(ID);
+
+			/*
+			for (int i = 0; i < PIECES; i++)
+			{
+				QuickSort(array, start[i], end[i]);
+				//printf("PARTION %d SORTED\n", i);
+			}
+			*/
+			while(!validEnd)
+			{
+				if(gettimeofday(&sortTimeEnd, NULL) != 0)
+				{
+					continue;
+				}
+				if(sortTimeEnd.tv_sec > 0 || (sortTimeEnd.tv_sec == 0 && sortTimeEnd.tv_usec > 0)) {
+					validEnd = true;
+				}
+			}
+			validEnd = false;
+			times(&CPU_TimeEnd);
+
+	    //gettimeofday(&sortTimeEnd, NULL);
+
+		//gettimeofday(&totalTimeEnd, NULL);
 	}
-	// NON MULTITHREADING
 	else
 	{
+		gettimeofday(&sortTimeStart, NULL);
+		while(!validStart)
+		{
+			if(gettimeofday(&sortTimeStart, NULL) != 0)
+			{
+				continue;
+			}
+			if(sortTimeStart.tv_sec > 0 || (sortTimeStart.tv_sec == 0 && sortTimeStart.tv_usec > 0)) {
+				validStart = true;
+			}
+		}
+		validStart = false;
+		times(&CPU_TimeStart);
 		QuickSort(array, low, high); // QUICK SORTING
-		//printf("NO MULTITHREAD\n\n");
+		while(!validEnd)
+		{
+			if(gettimeofday(&sortTimeEnd, NULL) != 0)
+			{
+				continue;
+			}
+			if(sortTimeEnd.tv_sec > 0 || (sortTimeEnd.tv_sec == 0 && sortTimeEnd.tv_usec > 0)) {
+				validEnd = true;
+			}
+		}
+		validEnd = false;
+		//gettimeofday(&sortTimeEnd, NULL);
+		//gettimeofday(&totalTimeEnd, NULL);
 	}
-	//if(!isSorted(array, SIZE)) perror("ERROR: List not sorted\n");
+	if(isSorted(array, SIZE) == false) 
+	{
+		perror("ERROR: List is not sorted\n");
+	}
+	free(array);
+	times(&CPU_TimeEnd);
+	while(!validEnd)
+	{
+		if(gettimeofday(&totalTimeEnd, NULL) != 0)
+		{
+			continue;
+		}
+		if(totalTimeEnd.tv_sec > 0 || (totalTimeEnd.tv_sec == 0 && totalTimeEnd.tv_usec > 0)) {
+			validEnd = true;
+		}
+	}
+		validEnd = false;
+	
 	printf("\n\nPOST SORTING\n");
+	totalLoadTime = (double)(loadTimeEnd.tv_sec - loadTimeStart.tv_sec) + ((loadTimeEnd.tv_usec - loadTimeStart.tv_usec) / 1000000.0);
+	totalSortTime = (double)(sortTimeEnd.tv_sec - sortTimeStart.tv_sec) + ((sortTimeEnd.tv_usec - sortTimeStart.tv_usec) / 1000000.0);
+	totalCPUTime = (double)(CPU_TimeEnd.tms_utime + CPU_TimeEnd.tms_cstime - CPU_TimeStart.tms_utime - CPU_TimeStart.tms_stime)/ sysconf(_SC_CLK_TCK);
+	totalTotalTime = (double)(totalTimeEnd.tv_sec - totalTimeStart.tv_sec) + ((totalTimeEnd.tv_usec - totalTimeStart.tv_usec) / 1000000.0);
 
+	printf("Load: %0.3f  ", totalLoadTime);
+	printf("Sort (Wall/CPU):   %0.3f /   %0.3f  ", totalSortTime, totalCPUTime);
+	printf("Total: %0.3f\n", totalTotalTime);
 	/*
 	for(int i=0; i < SIZE; i++)
 	{
@@ -316,7 +522,7 @@ int main(int argc, char *argv[], char *env[])
 
 bool isSorted(int sArray[], int Num)
 {
-	for(int i = 0; i < Num; i++)
+	for(int i = 0; i < Num - 1; i++)
 	{
 		if(sArray[i] > sArray[i + 1])
 		{
@@ -435,4 +641,13 @@ void QuickSort(int qarray[], int lo, int hi)
 
 		QuickSort(qarray, j + 1, hi);
 	}
+}
+
+void *Threading(void *ThreadArray)
+{
+	_Thread *tThreading = (_Thread *)ThreadArray;
+	int high = tThreading->end;
+	int low = tThreading->start;
+	QuickSort(array, low, high);
+	pthread_exit(NULL);
 }
